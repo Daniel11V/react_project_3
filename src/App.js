cca58f508f76f932259ca6ad50e66c843f7e973d
Daniel Vinet <daniel1vinet@Gmail.com>
2021-08-20 14:57:50 -0300

    Fixing deploy

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import { Provider } from 'react-redux';
 import store from './store';
-import Drum from './components/Drum.jsx'
+import Drum from './components/Drum.jsx';
 
 function App() {
   return (
--- src/components/Drum.jsx
+++ src/components/Drum.jsx
@@ -1,3 +1,4 @@
+import React from "react";
 import { connect } from 'react-redux';
 import DrumButton from './DrumButton.jsx';
 import { buttonsOne, buttonsTwo } from '../store.js';
